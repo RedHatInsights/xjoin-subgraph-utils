12f5e8da77fc9748351f2b163e2897cee89f8dce
ckyrouac <ckyrouac@redhat.com>
2022-03-18 10:40:47 -0400

    Add graphql query functions

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,3 +1,4 @@
 export * from "./types.js"
 export * from "./graphqlschema.js"
 export * from "./default.filters.js"
+export * from "./query.js"
---
+++ src/graphql/query.ts
@@ -0,0 +1,48 @@
+import {inputName} from "../avro";
+import {GraphqlSchema} from "./graphqlschema";
+import {SelectionNode} from "graphql";
+
+//recursive function to build list of fields that will be included in the elasticsearch response
+export function graphqlSelectionToESSourceFields(selectionSet: Readonly<SelectionNode[]>, sourceFields: string[], parent: string[]): string[] {
+    for (const dataFieldSelection of selectionSet) {
+        if (dataFieldSelection.kind !== 'Field') {
+            throw new Error('invalid selection');
+        }
+
+        if (dataFieldSelection.selectionSet !== undefined) {
+            const newParent = [...parent];
+            newParent.push(dataFieldSelection.name.value)
+            sourceFields = this.graphqlSelectionToESSourceFields(
+                dataFieldSelection.selectionSet.selections, sourceFields, newParent);
+        } else {
+            if (parent.length > 0) {
+                sourceFields.push(parent.join('.') + '.' + dataFieldSelection.name.value);
+            } else {
+                sourceFields.push(dataFieldSelection.name.value);
+            }
+        }
+    }
+
+    return sourceFields;
+}
+
+//recursive function to build elasticsearch filters
+export function graphqlFiltersToESFilters(parent: string[], queryFilters: Record<any, any>, esFilters: Record<any, any>[], schema: GraphqlSchema): Record<any, any>[] {
+    const parentInputSchema = schema.getInput(inputName(parent[parent.length - 1]));
+
+    for (const queryFilterKey in queryFilters) {
+        const queryFilterType = parentInputSchema.getField(queryFilterKey).type;
+
+        if (schema.defaultFilters.hasFilter(queryFilterType)) {
+            const defaultFilter = schema.defaultFilters.getFilter(queryFilterType);
+            esFilters.push(defaultFilter.buildESFilter(queryFilterKey, queryFilters[queryFilterKey], parent.join('.')));
+        } else {
+            //recurse into nested filter
+            const newParent = [...parent];
+            newParent.push(queryFilterKey)
+            esFilters = this.graphqlFiltersToESFilters(newParent, queryFilters[queryFilterKey], esFilters, schema);
+        }
+    }
+
+    return esFilters;
+}
